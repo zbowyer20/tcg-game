6c82c949246909408e6a794c625da92466cd025a
Zak Bowyer <zak.bowyer@live.co.uk>
2016-11-16 15:43:32 +0000

    socket disconnection

--- server/app.js
+++ server/app.js
@@ -2,28 +2,30 @@
 
 var express = require('express');
 var http = require('http');
-
 var socket = require('./routes/socket.js');
 
 var app = express();
 var server = http.createServer(app);
 
-app.set('views', __dirname + 'src');
-app.use(express.static(__dirname + '/public'));
 app.set('port', 3535);
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-}
-
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', function(socket) {
-  socket.emit('init', {socket: socket.id});
-  console.log("Got new connection from: " + socket.id);
-});
 
 server.listen(app.get('port'), function() {
   console.log('Express server listening on port %d in $s mode', app.get('port'), app.get('env'))
+});
+
+var io = require('socket.io').listen(server);
+
+var SOCKET_LIST = {};
+
+io.sockets.on('connection', function(socket) {
+  console.log("Got new connection from: " + socket.id);
+  SOCKET_LIST[socket.id] = socket;
+
+  socket.on('disconnect', function() {
+    delete SOCKET_LIST[socket.id];
+  })
+
+  socket.emit('init', {socket: socket.id});
 });
 
 module.exports = app;
