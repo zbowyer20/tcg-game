be57cad13b434228e68efd8cb47f4b95e96ae61e
Zak Bowyer <zak.bowyer@live.co.uk>
2017-01-05 10:03:20 +0000

    socket commenting

--- server/Socket.js
+++ server/Socket.js
@@ -4,6 +4,12 @@ var Sockets = {
   list: {},
 
   send: {
+
+    /**
+    * Send the initial state of the game
+    * @param game {Game}
+    * @emits {Object} containing the game to all players
+    */
     initial: function(game) {
       for (var i = 0; i < game.players.ids.list.length; i++) {
         let pid = game.players.ids.list[i];
@@ -16,6 +22,13 @@ var Sockets = {
       }
     },
 
+    /**
+    * Send public details of a player to their opponent and all their details
+    * to themself
+    * @param game {Game} The full game
+    * @emits {Object} containing the player's public data to their opponent
+    * @emits {Object} containing the player's full data to themself
+    */
     player: function(game) {
       let pids = game.players.ids.list;
       for (var i = 0; i < pids.length; i++) {
@@ -30,6 +43,11 @@ var Sockets = {
       }
     },
 
+    /**
+    * Send phase information to all players
+    * @param data {Object} containing the phase data and who to send it to
+    * @emits {Object} containing the phase information
+    */
     phase: function(data) {
       Sockets.send.data(null, {
         phase: data.phase
@@ -38,16 +56,24 @@ var Sockets = {
       });
     },
 
+    /**
+    * Get the relevant data to send to a player based on given options and
+    * send it
+    * @param pid {String} The id of the player the data references
+    * @param game {Game} The full data of the game
+    * @param options {Object} configuration on what object to send
+    */
     message: function(pid, game, options) {
+      // get the relevant data based on configuration options
       function getData(pid, game, options) {
         let data = {};
-        if (options.field) {
+        if (options.field) { // sending the field data
           data.field = game.field.getPack(data.players.list);
         }
-        if (options.opponent) {
+        if (options.opponent) { // sending public data of the player
           data.opponent = game.players.list[pid].getPublicPack();
         }
-        if (options.self) {
+        if (options.self) { // sending all data of the player
           data.self = game.players.list[pid].getPack();
         }
         return data;
--- server/routes/Socket.js
+++ server/routes/Socket.js
@@ -10,6 +10,8 @@ function Socket(server, app, game) {
     // if game is ready, send players initial state of game
     game.initialize(Sockets.list);
 
+    // when receiving a flag from a player that they are ready, flag them
+    // in game
     socket.on('ready', function() {
       game.players.ready(socket.id);
     });
@@ -21,56 +23,89 @@ function Socket(server, app, game) {
     });
   });
 
-  app.get('/api/field/draw/:id', function(req, res) {
-    let playerId = req.params.id;
+  /**
+  * Draw a card
+  * @param pid {String} The id of the player drawing a card
+  * @returns {Object} containing the card drawn and the player's id
+  * @emits data to update the opponent the player's hand has updated
+  */
+  app.get('/api/field/draw/:pid', function(req, res) {
+    let pid = req.params.pid;
     res.send({
-      card: game.draw(playerId),
-      player: playerId
+      card: game.draw(pid),
+      player: pid
     });
-    game.emitData(Sockets.list, {opponent: true}, playerId);
+    game.emitData(Sockets.list, {opponent: true}, pid);
   });
 
-  app.get('/api/field/discard/:playerId/:cardId', function(req, res) {
-    let playerId = req.params.playerId,
-        cardId = req.params.cardId,
-        data = game.discard(playerId, cardId);
+  /**
+  * Discard a card
+  * @param pid {String} The id of the player discarding a card
+  * @param cid {String} The id of the card to be discarded
+  * @returns {Object} containing the discarding player's id, the card discarded,
+                      their updated cp and what zone the card is moving to
+  * @emits data to update the opponent the player's hand has updated and
+           that the field has been updated
+  */
+  app.get('/api/field/discard/:pid/:cid', function(req, res) {
+    let pid = req.params.pid,
+        cid = req.params.cid,
+        data = game.discard(pid, cid);
 
     res.send({
-      player: playerId,
+      player: pid,
       card: data.card,
       cp: data.cp,
       to: "break"
     });
 
-    game.emitData(Sockets.list, {opponent: true, field: true}, playerId);
+    game.emitData(Sockets.list, {opponent: true, field: true}, pid);
   });
 
-  app.get('/api/field/play/:playerId/:cardId', function(req, res) {
-    let playerId = req.params.playerId,
-        cardId = req.params.cardId,
-        move = game.playCard(playerId, cardId);
+  /**
+  * Play a card
+  * @param pid {String} The id of the player playing a card
+  * @param cid {String} The id of the card to be played
+  * @returns {Object} containing the card being played, what zone it is being
+                      played to, the id of the player playing the card and their
+                      updated cp
+  * @emits data to update the opponent the player's hand has updated and that
+           the field has been updated
+  */
+  app.get('/api/field/play/:pid/:cid', function(req, res) {
+    let pid = req.params.pid,
+        cid = req.params.cid,
+        move = game.playCard(pid, cid);
 
     res.send({
       card: move.card,
       to: move.card.type,
-      player: playerId,
+      player: pid,
       cp: move.cp
     });
 
-    game.emitData(Sockets.list, {opponent: true, field: true}, playerId);
+    game.emitData(Sockets.list, {opponent: true, field: true}, pid);
   });
 
-  app.get('/api/field/dull/:playerId/:cardId', function(req, res) {
-    let playerId = req.params.playerId,
-        cardId = req.params.cardId,
-        card = game.dullCard(playerId, cardId);
+  /**
+  * Dull a card on the field
+  * @param pid {String} The id of the player dulling a card
+  * @param cid {String} The id of the card to be dulled
+  * @returns {Object} containing the dulled card and the id of the player
+                      who dulled it
+  * @emits data to update the field
+  */
+  app.get('/api/field/dull/:pid/:cid', function(req, res) {
+    let pid = req.params.pid,
+        cid = req.params.cid,
+        card = game.dullCard(pid, cid);
 
     res.send({
       card: card,
-      player: playerId
+      player: pid
     });
 
-    game.emitData(Sockets.list, {field: true}, playerId);
+    game.emitData(Sockets.list, {field: true}, pid);
   });
 }
 
